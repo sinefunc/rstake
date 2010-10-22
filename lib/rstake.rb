567a90fe8abe8dd1c49e4d1bcf3235d138574a12
Rico Sta. Cruz <rico@ambiescent.com>
2010-10-22 16:10:58 +0800

    Add a message about growl.

--- lib/rstake.rb
+++ lib/rstake.rb
@@ -1,7 +1,13 @@
 module RStake
   PREFIX = File.join(File.dirname(__FILE__), 'rstake')
   autoload :App,           "#{PREFIX}/app"
-  autoload :GrowlNotifier, "#{PREFIX}/growl_notifier"
   autoload :PrintNotifier, "#{PREFIX}/print_notifier"
   autoload :Watcher,       "#{PREFIX}/watcher"
+end
+
+
+begin
+  require 'growl'
+  require "#{RStake::PREFIX}/growl_notifier"
+rescue LoadError
 end
